32288571d3d2c669410b10cc37bd806204624e62
isaiah <isaiahmartin847@gmail.com>
2023-12-19 21:24:22 -0700

    added a methon to the class

--- stanley_bottle.py
+++ stanley_bottle.py
@@ -1,6 +1,18 @@
 class stanley:
-    def __init__(self, volume, color, hieght, material):
+    def __init__(self, product, volume, color, material, DOMF):
         self.volume = volume
         self.color = color
-        self.hieght = hieght
         self.material = material
+        self.product = product
+        self.DOMF = DOMF
+
+
+    def make_and_modle(self):
+        print(f"was mad in {self.DOMF}, color is {self.color}")
+
+
+
+
+bottle = stanley("Ice Flow", 30, "Orange", "steel", "A 23")
+
+bottle.make_and_modle()
