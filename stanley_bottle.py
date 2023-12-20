6246deb76e176d49b5412ca70ccd008f99b27b1c
isaiah <isaiahmartin847@gmail.com>
2023-12-19 21:08:31 -0700

    stanley class

---
+++ stanley_bottle.py
@@ -0,0 +1,6 @@
+class stanley:
+    def __init__(self, volume, color, hieght, material):
+        self.volume = volume
+        self.color = color
+        self.hieght = hieght
+        self.material = material
