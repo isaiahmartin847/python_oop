94568e0735f0ae2e7faae849936286969b257da3
isaiah <isaiahmartin847@gmail.com>
2023-12-19 21:37:57 -0700

    added a new element to the class and learned that you can splice strings

--- stanley_bottle.py
+++ stanley_bottle.py
@@ -10,10 +10,14 @@ class stanley:
     def make_and_dom(self):
         print(f"modle is = {self.product} and made in = {self.DOM}")
 
+    def get_month(self):
+        self.y = self.DOM[0]
+        print(self.y)
+
 
 
 
 ice_flow = stanley("Ice Flow", 30, "Orange", "steel", "A 23")
 quencher = stanley("Quencher", 40, "blue", "steel", "L 22")
-ice_flow.make_and_dom()
-quencher.make_and_dom()
+ice_flow.get_month()
+quencher.get_month()
